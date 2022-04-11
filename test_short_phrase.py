25802fae05eab3d9345e65d74d107b93910bd189
Sergei Garibzyanov <garibzyanovsergei@gmail.com>
2022-04-11 23:35:39 +0500

    add minor fix

--- test_short_phrase.py
+++ test_short_phrase.py
@@ -1,4 +1,4 @@
 class TestShortPhase:
-    def test_short_phrase(self):
+    def test_print_short_phrase(self):
         phrase = input('Set a phrase: ')
         assert len(phrase) < 15, 'This phrase is longer than 15 characters'
