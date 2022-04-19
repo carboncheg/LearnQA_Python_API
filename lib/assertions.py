52366544feae3da8bd68f3d182639367f112033e
Sergei Garibzyanov <garibzyanovsergei@gmail.com>
2022-04-19 22:05:19 +0500

    add def test_create_user_successfully()

--- lib/assertions.py
+++ lib/assertions.py
@@ -15,3 +15,19 @@ class Assertions:
         assert name in response_as_dict, f'Response JSON doesn\'t have key "{name}"'
         # Проверка значения
         assert response_as_dict[name] == expected_value, error_message
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        # Проверка формата
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f'Response is not in JSON format. Response text is "{response.text}"'
+
+        # Проверка ключа
+        assert name in response_as_dict, f'Response JSON doesn\'t have key "{name}"'
+
+    @staticmethod
+    def assert_code_status(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, \
+            f"Unexpected status code! Expected: '{expected_status_code}'. Actual: '{response.status_code}'."
